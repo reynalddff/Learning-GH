f59ec415a166a5b2bc03111ab6b5fce484df5819
Reynald Daffa Pahlevi <reynalddaffa.dev@gmail.com>
2021-05-20 08:17:54 +0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Reynald Daffa Pahlevi on 20/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
